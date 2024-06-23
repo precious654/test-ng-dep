00e4c187316bf4539106ba8a37f1157321037c83
Precious <odukwechiemeka@gmail.com>
2024-06-23 04:36:49 +0100

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import Parse from 'parse';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dep-app';
+  
+  question: number = 0;
+  questionAnswer: string = "";
+  questionNo: number = 1;
+
+  data: any = [];
+
+  fetchData = async () => {
+    try {
+      const query = new Parse.Query("Quiz");
+      const quiz = await query.find();
+      this.data = quiz;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  handleSubmit = () => {
+    if(this.questionAnswer === this.data[this.question].get("answer")) {
+      this.question = this.question + 1;
+      this.questionNo = this.questionNo + 1;
+      alert("correct");
+      this.fetchData();
+    } else {
+      alert("Wrong answer!!! Try again");
+    }
+  }
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
 }
+
+Parse.initialize("Nk6O6jwBL2mStRBnYnrSsqEfz28to8bBC7B7tNjB", "aeTiH8yTBPnJsU06rn96n1en6axhon0TTWGuzjAc");
+Parse.serverURL = "https://parseapi.back4app.com/";
